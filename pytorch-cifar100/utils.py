2051500712d342ce4f8b2d91cbba3c261ef3104b
Ritwick Sundar <ritwick.sundar@gmail.com>
2020-08-17 10:19:51 +0100

    Store only VGG and ResNet

--- pytorch-cifar100/utils.py
+++ pytorch-cifar100/utils.py
@@ -24,33 +24,6 @@ def get_network(args):
     elif args.net == 'vgg19':
         from models.vgg import vgg19_bn
         net = vgg19_bn()
-    elif args.net == 'densenet121':
-        from models.densenet import densenet121
-        net = densenet121()
-    elif args.net == 'densenet161':
-        from models.densenet import densenet161
-        net = densenet161()
-    elif args.net == 'densenet169':
-        from models.densenet import densenet169
-        net = densenet169()
-    elif args.net == 'densenet201':
-        from models.densenet import densenet201
-        net = densenet201()
-    elif args.net == 'googlenet':
-        from models.googlenet import googlenet
-        net = googlenet()
-    elif args.net == 'inceptionv3':
-        from models.inceptionv3 import inceptionv3
-        net = inceptionv3()
-    elif args.net == 'inceptionv4':
-        from models.inceptionv4 import inceptionv4
-        net = inceptionv4()
-    elif args.net == 'inceptionresnetv2':
-        from models.inceptionv4 import inception_resnet_v2
-        net = inception_resnet_v2()
-    elif args.net == 'xception':
-        from models.xception import xception
-        net = xception()
     elif args.net == 'resnet18':
         from models.resnet import resnet18
         net = resnet18()
@@ -66,70 +39,6 @@ def get_network(args):
     elif args.net == 'resnet152':
         from models.resnet import resnet152
         net = resnet152()
-    elif args.net == 'preactresnet18':
-        from models.preactresnet import preactresnet18
-        net = preactresnet18()
-    elif args.net == 'preactresnet34':
-        from models.preactresnet import preactresnet34
-        net = preactresnet34()
-    elif args.net == 'preactresnet50':
-        from models.preactresnet import preactresnet50
-        net = preactresnet50()
-    elif args.net == 'preactresnet101':
-        from models.preactresnet import preactresnet101
-        net = preactresnet101()
-    elif args.net == 'preactresnet152':
-        from models.preactresnet import preactresnet152
-        net = preactresnet152()
-    elif args.net == 'resnext50':
-        from models.resnext import resnext50
-        net = resnext50()
-    elif args.net == 'resnext101':
-        from models.resnext import resnext101
-        net = resnext101()
-    elif args.net == 'resnext152':
-        from models.resnext import resnext152
-        net = resnext152()
-    elif args.net == 'shufflenet':
-        from models.shufflenet import shufflenet
-        net = shufflenet()
-    elif args.net == 'shufflenetv2':
-        from models.shufflenetv2 import shufflenetv2
-        net = shufflenetv2()
-    elif args.net == 'squeezenet':
-        from models.squeezenet import squeezenet
-        net = squeezenet()
-    elif args.net == 'mobilenet':
-        from models.mobilenet import mobilenet
-        net = mobilenet()
-    elif args.net == 'mobilenetv2':
-        from models.mobilenetv2 import mobilenetv2
-        net = mobilenetv2()
-    elif args.net == 'nasnet':
-        from models.nasnet import nasnet
-        net = nasnet()
-    elif args.net == 'attention56':
-        from models.attention import attention56
-        net = attention56()
-    elif args.net == 'attention92':
-        from models.attention import attention92
-        net = attention92()
-    elif args.net == 'seresnet18':
-        from models.senet import seresnet18
-        net = seresnet18()
-    elif args.net == 'seresnet34':
-        from models.senet import seresnet34
-        net = seresnet34()
-    elif args.net == 'seresnet50':
-        from models.senet import seresnet50
-        net = seresnet50()
-    elif args.net == 'seresnet101':
-        from models.senet import seresnet101
-        net = seresnet101()
-    elif args.net == 'seresnet152':
-        from models.senet import seresnet152
-        net = seresnet152()
-
     else:
         print('the network name you have entered is not supported yet')
         sys.exit()
@@ -160,7 +69,6 @@ def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=Tru
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
     ])
-    #cifar100_training = CIFAR100Train(path, transform=transform_train)
     cifar100_training = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
 
     # Sanity check: Check to see whether shuffled_dataset works with default ordering of CIFAR-100
@@ -200,7 +108,6 @@ def get_test_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
         transforms.ToTensor(),
         transforms.Normalize(mean, std)
     ])
-    #cifar100_test = CIFAR100Test(path, transform=transform_test)
     cifar100_test = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
     cifar100_test_loader = DataLoader(cifar100_test, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
 
