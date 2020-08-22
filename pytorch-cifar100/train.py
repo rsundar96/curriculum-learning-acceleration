ac286827443c4b0759831af87c0b6b8a4ab8e5e3
Ritwick Sundar <ritwick.sundar@gmail.com>
2020-08-22 01:49:38 +0100

    Add transfer learning and curriculum ordering

--- pytorch-cifar100/train.py
+++ pytorch-cifar100/train.py
@@ -1,11 +1,10 @@
-# train.py
 #!/usr/bin/env	python3
 
 import os
 import sys
 import argparse
 import time
-from datetime import datetime
+import pickle
 
 import numpy as np
 import torch
@@ -14,13 +13,37 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 
+from datetime import datetime
 from torch.utils.data import DataLoader
 
 from conf import settings
 from utils import get_network, get_training_dataloader, get_test_dataloader, WarmUpLR
 
-def train(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list):
 
+def curriculum_img_order():
+    # List to store final loss of 50,000 images
+    idx2Scores = []
+
+    # Obtain the ordering
+    with torch.no_grad():
+        net.eval()
+
+        for image, label in curriculum_dataloader:
+            label = label.cuda()
+            image = image.cuda()
+
+            output = net(image)
+            loss = cl_loss_function(output, label)
+            idx2Scores.append(loss.item())
+
+    # Pickle idx2Scores so that it can be read in utils.py to order the dataset
+    with open('cifar100_img_losses', 'wb') as fp:
+        pickle.dump(idx2Scores, fp)
+
+    return idx2Scores
+
+
+def train(epoch, pretrained, train_loss_list, train_acc_list, test_loss_list, test_acc_list):
     start = time.time()
     net.train()
 
@@ -28,9 +51,6 @@ def train(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list)
     train_loss_epoch = 0.0
     train_acc_epoch = 0.0
     correct = 0.0
-
-    # List to store final loss of 50,000 images
-    idx2Scores = []
 
     for batch_index, (images, labels) in enumerate(cifar100_training_loader):
         if epoch <= args.warm:
@@ -50,11 +70,7 @@ def train(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list)
         correct += preds.eq(labels).sum()
 
         # Store training loss and accuracy
-        train_loss_epoch = loss.item()
-        train_acc_epoch = correct.float() / len(cifar100_training_loader.dataset)
-
-        if epoch == 199:
-            idx2Scores.append(loss.item())
+        train_loss_epoch += loss.item()
 
         print('Training Epoch: {epoch} [{trained_samples}/{total_samples}]\tLoss: {:0.4f}\tLR: {:0.6f}'.format(
             loss.item(),
@@ -64,27 +80,34 @@ def train(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list)
             total_samples=len(cifar100_training_loader.dataset)
         ))
 
+    # Obtain the losses of each individual image in the dataset
+    # Only execute when running the pretrained network with last layer being trained AND
+    # When last epoch is being run
+    if (epoch == settings.EPOCH - 1) and pretrained:
+        curriculum_img_order()
+
     finish = time.time()
+
+    # Store the values of the training loss and accuracy
+    train_loss_epoch = train_loss_epoch / len(cifar100_training_loader.dataset)
+    train_acc_epoch = correct.float() / len(cifar100_training_loader.dataset)
 
     train_loss_list.append(train_loss_epoch)
     train_acc_list.append(train_acc_epoch.item())
 
     print('epoch {} training time consumed: {:.2f}s'.format(epoch, finish - start))
 
-    # if epoch == 199:
-    #     print('idx2Scores list: ', idx2Scores)
 
 @torch.no_grad()
 def eval_training(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list):
-
     start = time.time()
     net.eval()
 
-    test_loss = 0.0 # cost function error
+    test_loss = 0.0  # cost function error
     correct = 0.0
+    avg_test_loss = 0.0
 
     for (images, labels) in cifar100_test_loader:
-
         images = images.cuda()
         labels = labels.cuda()
 
@@ -97,19 +120,20 @@ def eval_training(epoch, train_loss_list, train_acc_list, test_loss_list, test_a
     finish = time.time()
 
     print('Evaluating Network.....')
-    print('Test set: Average loss: {:.4f}, Loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
-        test_loss / len(cifar100_test_loader.dataset),
-        test_loss,
+    print('[TEST SET] Avg. Loss: {:.4f}, Accuracy: {:.4f}, Time consumed:{:.2f}s'.format(
+        test_loss / len(cifar100_test_loader),
         correct.float() / len(cifar100_test_loader.dataset),
         finish - start
     ))
 
+    avg_test_loss = test_loss / len(cifar100_test_loader.dataset)
     test_accuracy = correct.float() / len(cifar100_test_loader.dataset)
 
-    test_loss_list.append(test_loss)
+    test_loss_list.append(avg_test_loss)
     test_acc_list.append(test_accuracy.item())
 
     return correct.float() / len(cifar100_test_loader.dataset)
+
 
 if __name__ == '__main__':
 
@@ -119,18 +143,42 @@ if __name__ == '__main__':
     parser.add_argument('-b', type=int, default=128, help='batch size for dataloader')
     parser.add_argument('-warm', type=int, default=1, help='warm up training phase')
     parser.add_argument('-lr', type=float, default=0.1, help='initial learning rate')
-    # parser.add_argument('-curriculum', type=bool, default=False, help='curriculum initialisation')
+    parser.add_argument('-curriculum', type=bool, default=False, help='curriculum initialisation')
+    parser.add_argument('-pretrained', type=bool, default=False, help='train only last layer')
     args = parser.parse_args()
 
     net = get_network(args)
 
-    #data preprocessing:
+    if args.pretrained:
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # Load the location of the pretrained weights here
+        weights_path = './checkpoint/vgg19/Friday_21_August_2020_22h_19m_33s/vgg19-89-best.pth'
+        net.load_state_dict(torch.load(weights_path), args.gpu)
+
+        for param in net.parameters():
+            param.requires_grad = False
+
+        num_ftrs = net.classifier[6].in_features
+        net.classifier[6] = nn.Linear(num_ftrs, 100)
+
+        net = net.to(device)
+
+        # Optimize only last layer
+        optimizer = optim.SGD(net.classifier[6].parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+    else:
+        # Optimize the entire network
+        optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+    # Data preprocessing (Train and Test DataLoaders)
     cifar100_training_loader = get_training_dataloader(
         settings.CIFAR100_TRAIN_MEAN,
         settings.CIFAR100_TRAIN_STD,
         num_workers=4,
         batch_size=args.b,
-        shuffle=True
+        shuffle=False,
+        curriculum=args.curriculum
     )
 
     cifar100_test_loader = get_test_dataloader(
@@ -141,14 +189,28 @@ if __name__ == '__main__':
         shuffle=True
     )
 
+    # Curriculum DataLoader
+    if args.pretrained:
+        curriculum_dataloader = get_training_dataloader(
+            settings.CIFAR100_TRAIN_MEAN,
+            settings.CIFAR100_TRAIN_STD,
+            num_workers=4,
+            batch_size=1,
+            shuffle=False,
+            curriculum=False
+        )
+
+        # Loss Function for Curriculum Ordering
+        cl_loss_function = nn.CrossEntropyLoss(reduction='none')
+
     loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
-    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES, gamma=0.2) #learning rate decay
+    train_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=settings.MILESTONES,
+                                                     gamma=0.2)  # learning rate decay
     iter_per_epoch = len(cifar100_training_loader)
     warmup_scheduler = WarmUpLR(optimizer, iter_per_epoch * args.warm)
     checkpoint_path = os.path.join(settings.CHECKPOINT_PATH, args.net, settings.TIME_NOW)
 
-    #create checkpoint folder to save model
+    # Create checkpoint folder to save model
     if not os.path.exists(checkpoint_path):
         os.makedirs(checkpoint_path)
     checkpoint_path = os.path.join(checkpoint_path, '{net}-{epoch}-{type}.pth')
@@ -164,10 +226,10 @@ if __name__ == '__main__':
         if epoch > args.warm:
             train_scheduler.step(epoch)
 
-        train(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list)
+        train(epoch, args.pretrained, train_loss_list, train_acc_list, test_loss_list, test_acc_list)
         acc = eval_training(epoch, train_loss_list, train_acc_list, test_loss_list, test_acc_list)
 
-        #start to save best performance model after learning rate decay to 0.01
+        # Start to save best performance model after learning rate decay to 0.01
         if epoch > settings.MILESTONES[1] and best_acc < acc:
             torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='best'))
             best_acc = acc
@@ -176,6 +238,7 @@ if __name__ == '__main__':
         if not epoch % settings.SAVE_EPOCH:
             torch.save(net.state_dict(), checkpoint_path.format(net=args.net, epoch=epoch, type='regular'))
 
+    # Print only for graphing purpose
     print("\nTraining Loss: ", train_loss_list)
     print("\nTest Loss: ", test_loss_list)
     print("\nTraining Accuracy: ", train_acc_list)
